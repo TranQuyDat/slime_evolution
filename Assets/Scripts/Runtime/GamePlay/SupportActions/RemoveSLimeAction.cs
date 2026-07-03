4582bc2f88c3b34670517e9c0bacea249019b9a0
TranQuyDat <datraumuong@gmail.com>
2026-07-04 06:24:56 +0700

    update material slime outline && add highlight unhightlight slime in RemoveSlimeAction.cs

--- Assets/Scripts/Runtime/GamePlay/SupportActions/RemoveSLimeAction.cs
+++ Assets/Scripts/Runtime/GamePlay/SupportActions/RemoveSLimeAction.cs
@@ -10,6 +10,7 @@ class RemoveSlimeAction : SupportAction
     private Slime _hoveredSlime;
     private const int MaxSlimes_To_Remove = 3;
     private Camera _camera;
+    bool IsSlimeHoverInRemoveList => _slimesToRemove.Contains(_hoveredSlime);
     public RemoveSlimeAction(PitController pitCtrl,InputSystem inputSystem)
     {
         _gameManager = GameManager.Instance;
@@ -20,27 +21,35 @@ class RemoveSlimeAction : SupportAction
     }
     public override void OnUpdate()
     {
-        if(!IsMouseInPit()
-        || _slimesToRemove.Count >= MaxSlimes_To_Remove) return;
+        if(!IsMouseInPit()) return;
 
         Vector3 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0f;
         
         if(_inputSystem.TryRaycastMouse2D<Slime>(mousePos,out Slime slime,LayerMask.GetMask("Slime")))
         {
-            if(_hoveredSlime != null && _hoveredSlime != slime)
+            if(_hoveredSlime != null && _hoveredSlime != slime 
+            && !IsSlimeHoverInRemoveList)
             {
                 UnHighlightSlime(_hoveredSlime);
             }
             _hoveredSlime = slime;
             HighlightSlime(slime);
         }
-        else _hoveredSlime = null;
+        else if(_hoveredSlime != null && !IsSlimeHoverInRemoveList)
+        {
+            UnHighlightSlime(_hoveredSlime);
+            _hoveredSlime = null;
+        }
+        else if(slime == null && _hoveredSlime != null)
+        {
+            _hoveredSlime = null;
+        }
     }
 
     public override void OnEnter()
     {
-        _inputSystem.BindAction(KeyCode.Mouse0,AddSlimeToRemovalList);
+        _inputSystem.BindAction(KeyCode.Mouse0,HandleBindRemoveAddSlime);
         _gameManager.Hud.SendCommand(CommandType.UpdateRemoveSlimesText,0);
     }
 
@@ -51,17 +60,19 @@ class RemoveSlimeAction : SupportAction
             OnFinish();
             return;
         }
-        foreach(var slime in _slimesToRemove)
+        foreach(Slime slime in _slimesToRemove)
         {
+            UnHighlightSlime(slime);
             slime.Destroy();
         }
         OnFinish();
     }
     public override void OnFinish()
     {
-        _slimesToRemove.Clear();
-        _inputSystem.UnbindAction(KeyCode.Mouse0,AddSlimeToRemovalList);
         _hoveredSlime = null;
+        _slimesToRemove.Clear();
+        _inputSystem.UnbindAction(KeyCode.Mouse0,HandleBindRemoveAddSlime);
+        
     }
 
     private bool IsMouseInPit()
@@ -71,16 +82,35 @@ class RemoveSlimeAction : SupportAction
         return _pitCtrl.Bounds.Contains(mousePos);
     }
 
+    private void HandleBindRemoveAddSlime()
+    {
+        if(_hoveredSlime == null ) return;
+        if (!IsSlimeHoverInRemoveList)
+        {
+            AddSlimeToRemovalList();
+        }
+        else
+        {
+            RemoveSlimeFromRemovalList();
+        }
+    }
+
     private void AddSlimeToRemovalList()
     {
-        if(_hoveredSlime == null) return;
         if(_slimesToRemove.Count >= MaxSlimes_To_Remove)
         {
             _hoveredSlime = null;
             return;
         }
-        if(_slimesToRemove.Contains(_hoveredSlime)) return;
         _slimesToRemove.Add(_hoveredSlime);
+        _gameManager.Hud.SendCommand(CommandType.UpdateRemoveSlimesText,
+        _slimesToRemove.Count);
+
+    }
+    private void RemoveSlimeFromRemovalList()
+    {
+        if(_hoveredSlime)
+        _slimesToRemove.Remove(_hoveredSlime);
         _gameManager.Hud.SendCommand(CommandType.UpdateRemoveSlimesText,
         _slimesToRemove.Count);
 
@@ -88,9 +118,11 @@ class RemoveSlimeAction : SupportAction
     private void HighlightSlime(Slime slime)
     {
         if(slime == null) return;
+        slime.Material.SetFloat("_UseOutline", 1f);
     }
     private void UnHighlightSlime(Slime slime)
     {
         if(slime == null) return;
+        slime.Material.SetFloat("_UseOutline", 0f);
     }
 }
